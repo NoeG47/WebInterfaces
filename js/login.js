0fd24f845af680aa63dc107d5794e185a1a4d76e
NoeG47 <hellobrawlstars2.0@gmail.com>
2025-01-31 10:19:53 +0100

    tocar en casa

--- js/login.js
+++ js/login.js
@@ -1,3 +1,34 @@
+// Obtener elementos
+const userMenuButton = document.getElementById("user-menu-button");
+const userMenu = document.getElementById("user-menu");
+
+// Añadir evento de clic al botón (imagen)
+userMenuButton.addEventListener("click", function () {
+  // Alternar la visibilidad del menú
+  if (userMenu.style.display === "none" || userMenu.style.display === "") {
+    userMenu.style.display = "block"; // Mostrar menú
+  } else {
+    userMenu.style.display = "none"; // Ocultar menú
+  }
+});
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  const menu = document.getElementById("mobile-menu");
+  const openIcon = document.querySelector(".menu-icon-open");
+  const closeIcon = document.querySelector(".menu-icon-close");
+
+  // Alternar visibilidad del menú
+  menu.classList.toggle("hidden");
+
+  // Alternar iconos
+  openIcon.classList.toggle("hidden");
+  closeIcon.classList.toggle("hidden");
+
+  // Actualizar aria-expanded
+  const isExpanded = this.getAttribute("aria-expanded") === "true";
+  this.setAttribute("aria-expanded", !isExpanded);
+});
+
+//Funcion para ver la contraseña
 function togglePasswordVisibility() {
   const passwordInput = document.getElementById("password");
   const eyeIcon = document.getElementById("eye-icon");
